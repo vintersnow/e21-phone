47cc6aca4808a6dfd009b9c6db0b99e91bfba5af
izuku <ankasandora@gmail.com>
2015-07-06 10:13:50 +0900

    no message

--- ClientServerClass.h
+++ ClientServerClass.h
@@ -24,7 +24,6 @@ private:
     pthread_exit(NULL);
   };
 
-
 public:
   char name[MAX_NAME_LEN];
   int def_conn;
--- client.cpp
+++ client.cpp
@@ -206,10 +206,10 @@ void* my_send(void *args){
 	}
       }
     }
-// >>>>>>> master
-    // sample_to_complex(in_data,X,FN);
-    // fft(X,Y,FN);
-    // if((n=send(data->conn,Y+cut,BUFFER_SIZE,0))<0) error("send error");
+// >>>>>>> FFT 
+    sample_to_complex(in_data,X,FN);
+    fft(X,Y,FN);
+    if((n=send(data->conn,Y+cut,BUFFER_SIZE,0))<0) error("send error");
     if(send(data->conn,in_data,SEND_BUFFER,0)<0) error("send error");
     // if(send_all(data->conn,in_data,N)<0) error("send error");
   }
@@ -230,14 +230,14 @@ void* my_recv(void*args){
     memset(Y,0,FN*sizeof(complex<double>));
     // printf("start recv\n");
     // n = recv(data->conn,out_data,N,0);
-    // n=recv(data->conn,Y+cut,BUFFER_SIZE,0);
-    n = recv(data->conn,out_data,SEND_BUFFER,0);
+    n=recv(data->conn,Y+cut,BUFFER_SIZE,0);
+    // n = recv(data->conn,out_data,SEND_BUFFER,0);
     // printf("recv\n");
     // n = recv_all(data->conn,out_data,N);
     if(n<0) error("recv out_data error");
     if(n==0) break;
-    // ifft(Y,X,FN);
-    // complex_to_sample(X,out_data,FN);
+    ifft(Y,X,FN);
+    complex_to_sample(X,out_data,FN);
     fwrite(out_data,sizeof(short),N,data->fp);
   }
   return NULL;
