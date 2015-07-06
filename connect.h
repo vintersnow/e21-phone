454a6a10dc0d78b086a2f6e6c4f663a5a23d4d21
izuku <ankasandora@gmail.com>
2015-07-06 09:44:28 +0900

    N=512

--- connect.h
+++ connect.h
@@ -1,5 +1,5 @@
 #ifndef DEFF2015_6_29
-#define N 4096 //for window_func
+#define N 512 //for window_func
 // #define N 8192
 #define COMMAND "rec -q -t raw -b 16 -c 1 -e s -r 44100 -"
 #define COMMAND2 "play -q -t raw -b 16 -c 1 -e s -r 44100 -"
